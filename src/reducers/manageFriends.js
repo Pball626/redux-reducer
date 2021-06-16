0f442ebea93d488120677d4ed64150a41059922b
Jose Mendoza <pball626@gmail.com>
2021-06-16 17:12:32 -0500

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,40 @@
-export function manageFriends(state, action){
+const initialState = {
+    friendState: []
+}
+
+export function manageFriends(state = initialState, action) {
+    switch (action.type) {
+        case "ADD_FRIEND":
+            return {
+                "friends": [
+                    {
+                        "hometown": "NYC",
+                        "id": 100,
+                        "name": "Avi"
+                    },
+                    {
+                        "hometown": "Boston",
+                        "id": 101,
+                        "name": "Joe"
+                    }
+                ]
+            }
+        case "REMOVE_FRIEND":
+            return {
+                "friends": [
+                    {
+                        "hometown": "NYC",
+                        "id": 100,
+                        "name": "Avi"
+                    },
+                    {
+                        "hometown": "Philadephia",
+                        "id": 102,
+                        "name": "Steven"
+                      }
+                ]
+            }
+            default: 
+            return state
+    }
 }
